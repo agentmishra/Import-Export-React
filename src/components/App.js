9517f34f68d7b1e6c81c1418fd6a1550b4dd0267
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-02-08 00:57:01 +0530

    Create App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-import Heading from ./Heading;
-import InputQuery from ./InputQuery;
-import SubHeading from ./SubHeading;
-import SubmitButton from ./SubmitButton;
+import Heading from "./Heading";
+import InputQuery from "./InputQuery";
+import SubHeading from "./SubHeading";
+import SubmitButton from "./SubmitButton";
 
 
 const App = () => {
